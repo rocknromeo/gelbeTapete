eeb6caaa890e287528f796782cdaae7551c96c81
rocknromeo <super-dude@gmx.de>
2022-10-18 11:23:45 +0200

    aufräumen

--- AnalyseScript.R
+++ AnalyseScript.R
@@ -1,1 +1,13 @@
 # Analysescript des Teams GelbeTapete
+
+# Assign a value to the variable my_apples
+my_apples <- 5 
+
+# Fix the assignment of my_oranges
+my_oranges <- 6 
+
+# Create the variable my_fruit and print it out
+my_fruit <- my_apples + my_oranges 
+my_fruit
+
+class(myfruit)
